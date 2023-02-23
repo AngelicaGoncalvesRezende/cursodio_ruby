5f666ee19287125051cea56d4d69eaa4373b2bcd
Angélica Rezende <angel.rezende@outlook.com>
2023-02-22 23:13:50 -0300

    desafio biblioteca

---
+++ desafios/desafio1.rb
@@ -0,0 +1,15 @@
+puts "Digite o seu nome: "
+
+nome = gets.chomp
+
+puts "Digite o seu sobrenome: "
+
+sobrenome = gets.chomp
+
+puts "Digite a sua idade: "
+
+idade = gets.chomp.to_i
+
+
+puts "Já verificamos que você se chama #{nome} #{sobrenome} e tem #{idade} anos. 
+Obrigada por efetuar o cadastro."
